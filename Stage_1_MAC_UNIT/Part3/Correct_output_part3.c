9411d3991175c9fa6f9eb44c14c4188d6560bc1f
Deepak Phadtare <83227533+deepakcr7ms7@users.noreply.github.com>
2022-03-31 20:06:17 -0400

    Add files via upload

---
+++ Stage_1_MAC_UNIT/Part3/Correct_output_part3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <time.h>
+
+void main()
+{
+	int inputs = 1000;
+	//srand(time(NULL));
+
+	FILE* inputData, * expectedOutput;
+	inputData = fopen("input_data_part3.txt", "w");
+
+	expectedOutput = fopen("Expected_op_part3.txt", "w");
+	if (expectedOutput == NULL)
+	{
+		printf("file does not exist");
+
+	}
+
+	fprintf(expectedOutput, "x\n0\n0\n0\n");
+	int i, valid_in;
+	signed int  a, b;
+	signed int f;
+	f = 0;
+	for (i = 0; i < 1000; i++)
+	{
+		a = (rand() % 512) - 256;
+		b = (rand() % 512) - 256;
+		valid_in = rand() % 2;
+
+		if (valid_in == 1)
+		{
+			f = (f + (a * b));
+			if (f > 524287)
+			{
+				f = 524287;
+
+			}
+			else if (f < -524288)
+			{
+				f = -524288;
+			}
+
+
+
+		}
+		else
+		{
+			f = f;
+		}
+
+		//printf("%d %d %d\n",a,b,valid_in,f);
+		fprintf(inputData, "%x\n%x\n%x\n",((a >> 0) & 0x3ff), ((b >> 0) & 0x3ff), valid_in);
+		fprintf(expectedOutput, "%d\n", ((f >> 0) & 0xfffff));
+	}
+	printf("Done");
+	fclose(inputData);
+	fclose(expectedOutput);
+
+}
